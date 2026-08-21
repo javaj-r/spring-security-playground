002344ae81d2a1f5eedadbdacf61cf861cdf0b5b
Javid <javid.rafiee@gmail.com>
2026-08-21 14:03:19 +0330

    Add inspectSecurity exercise.

--- src/main/java/org/javid/spring/security/playground/config/SecurityConfig.java
+++ src/main/java/org/javid/spring/security/playground/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.javid.spring.security.playground.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,17 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    CommandLineRunner inspectSecurity(UserDetailsService userDetailsService) {
+        return _ -> {
+            IO.print("UserDetailsService = %s%n".formatted(userDetailsService.getClass()));
+
+            var user = userDetailsService.loadUserByUsername("user");
+            IO.print("User = %s%n".formatted(user));
+
+            IO.print("Authorities = %s%n".formatted(user.getAuthorities()));
+        };
     }
 }
